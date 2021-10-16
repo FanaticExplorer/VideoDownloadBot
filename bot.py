b53715f81d6d1fe0a769b7567f5cc619d6d74330
FanaticExplorer <andrey.gurtov@gmail.com>
2021-10-16 19:09:05 +0300

    Now showing user that error happened

--- bot.py
+++ bot.py
@@ -4,14 +4,12 @@ from aiogram.utils import executor
 from aiogram.utils.emoji import emojize
 import re
 
-from youtube_dl.utils import DownloadError
 
 import yt_download as yt
 from config import token
 
 bot = Bot(token=token)
 dp = Dispatcher(bot)
-
 
 
 @dp.message_handler(commands=['start'])
@@ -28,19 +26,27 @@ async def process_services_command(message: types.Message):
     await message.reply(emojize('Список всех доступных сайтов здесь :point_down: : \n https://github.com/ytdl-org/youtube-dl/blob/master/docs/supportedsites.md'))
 
 @dp.message_handler(content_types=['text'])
-async def echo_download_message(msg: types.Message):
+async def echo_download_message(message: types.Message):
+    echo_download=yt.Downloader(message.text)
     try:
-        echo_download=yt.Downloader(msg.text)
-    except DownloadError:
-        text_of_message=msg.text
-        if "\n" in msg.text:
-            text_of_message.replace("\n"," ",1)
-            text_of_message.replace("\r"," ",1)
-        link_from_message=re.search("(?P<url>https?://[^\s]+)", text_of_message).group("url")
-        echo_download=yt.Downloader(link_from_message)
-    videonote = open(echo_download.download_video(), 'rb')
-    await msg.answer_video(videonote)
+        videonote = open(echo_download.download_video(), 'rb')
+    # except:
+    #     try:
+    #         text_of_message=message.text
+    #         link_from_message=re.search("(?P<url>https?://[^\s]+)", text_of_message).group("url")
+    #         echo_download=yt.Downloader(link_from_message)
+    #     except:
+    #         await message.reply("К сожалению, произошла ошибка...\nПопробуйте вставить именно ссылку, без доп.текста!")
+    #         print('Failure finding the link')
+    #         return
+    except:
+        await message.reply("К сожалению, произошла ошибка...\n")
+        print('Error :(')
+        return
+    
+    await message.reply("Готово, видео скачано на сервер.\nНачинаю отправку вам...")
+    await message.answer_video(videonote)
     videonote.close()
 
-print("starting")
+print("Starting")
 executor.start_polling(dp)
