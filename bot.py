ce07a2d687871dad3e4bcd5da40defe003eefc3f
FanaticExplorer <andrey.gurtov@gmail.com>
2021-10-29 19:22:54 +0300

    Removed varient of another download of the file

--- bot.py
+++ bot.py
@@ -37,12 +37,6 @@ async def process_services_command(message: types.Message):
 
 @dp.message_handler(content_types=['text'])
 async def echo_download_message(message: types.Message):
-    global videonote
-    inline_btn_video = InlineKeyboardButton('Скачать как об.видео', callback_data='download_as_video')
-    inline_btn_file = InlineKeyboardButton('Скачать как файл(пока нерабочее -_-)', callback_data='download_as_file')
-    choose_inline = InlineKeyboardMarkup(row_width=1)
-    choose_inline.add(inline_btn_video)
-    choose_inline.add(inline_btn_file)
     echo_download=yt.Downloader(message.text)
     await message.reply("Увидел, начинаю скачивание...")
 
@@ -52,30 +46,15 @@ async def echo_download_message(message: types.Message):
         await message.reply("К сожалению, произошла ошибка...")
         print('Error :(')
         return
-    await message.reply("Готово, видео скачано на сервер.\nКак вы хотите получить это видео?", reply_markup=choose_inline)
-
-
-@dp.callback_query_handler(text='download_as_video')
-async def choose_inline_video_callback_handler(query: types.CallbackQuery):
-    await bot.send_message(query.from_user.id, 'Начинаю отправку')
-    sleep(0.5)
-    try:
-        await bot.send_video(query.from_user.id, videonote)
-    except:
-        await bot.send_message(query.from_user.id, "К сожалению, произошла ошибка при отправке...")
-    finally:
-        videonote.close()
-
-@dp.callback_query_handler(text='download_as_file')
-async def choose_inline_video_callback_handler(query: types.CallbackQuery):
-    await bot.send_message(query.from_user.id, 'Начинаю отправку')
-    sleep(0.5)
+    await message.reply("Готово, видео скачано на сервер.\nОтправляю...",)
     try:
         await bot.send_document(query.from_user.id, videonote)
     except:
         await bot.send_message(query.from_user.id, "К сожалению, произошла ошибка при отправке...")
     finally:
         videonote.close()
+
+
 
 
 
