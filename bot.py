b445a56e3c8f3ae6597a4d64a78866bc7cddff6c
FanaticExplorer <andrey.gurtov@gmail.com>
2021-11-12 16:39:05 +0200

    Added all files again
    Now i will push with github, not git

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
 from aiogram import Bot, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+import aiogram
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.utils.emoji import emojize
-import re
 
+import re
+import os
+
+from time import sleep
 
 import yt_download as yt
-from config import token
+from config import token,test_token
 
-bot = Bot(token=token)
+if "HEROKU" in list(os.environ.keys()):
+    bot = Bot(token=token)
+else:
+    bot = Bot(token=test_token)
+
 dp = Dispatcher(bot)
 
 
@@ -19,34 +28,34 @@ async def process_start_command(message: types.Message):
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("К сожалению, эта часть еще не готова...\nПодождите немного!")
+    await message.reply("К сожалению, эта часть еще не готова...\nПодождите немного!\nСкоро...")
 
 @dp.message_handler(commands=['available_services'])
 async def process_services_command(message: types.Message):
     await message.reply(emojize('Список всех доступных сайтов здесь :point_down: : \n https://github.com/ytdl-org/youtube-dl/blob/master/docs/supportedsites.md'))
 
+
 @dp.message_handler(content_types=['text'])
 async def echo_download_message(message: types.Message):
     echo_download=yt.Downloader(message.text)
+    await message.reply("Увидел, начинаю скачивание...")
+
     try:
         videonote = open(echo_download.download_video(), 'rb')
-    # except:
-    #     try:
-    #         text_of_message=message.text
-    #         link_from_message=re.search("(?P<url>https?://[^\s]+)", text_of_message).group("url")
-    #         echo_download=yt.Downloader(link_from_message)
-    #     except:
-    #         await message.reply("К сожалению, произошла ошибка...\nПопробуйте вставить именно ссылку, без доп.текста!")
-    #         print('Failure finding the link')
-    #         return
     except:
-        await message.reply("К сожалению, произошла ошибка...\n")
+        await message.reply("К сожалению, произошла ошибка...")
         print('Error :(')
         return
-    
-    await message.reply("Готово, видео скачано на сервер.\nНачинаю отправку вам...")
-    await message.answer_video(videonote)
-    videonote.close()
+    await message.reply("Готово, видео скачано на сервер.\nОтправляю...",)
+    try:
+        await bot.send_document(query.from_user.id, videonote)
+    except:
+        await bot.send_message(query.from_user.id, "К сожалению, произошла ошибка при отправке...")
+    finally:
+        videonote.close()
+
+
+
 
 print("Starting")
 executor.start_polling(dp)
